fcf137de7342897e580caed23b0972fb4f030fbc
Martin <slackgt@bigpond.com>
2025-02-22 17:21:03 +1100

    22022025-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import relativeLinks from 'astro-relative-links';
+// import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [relativeLinks()]
+  // integrations: [relativeLinks()]
 });
