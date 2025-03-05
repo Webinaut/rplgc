593f0c27c8558cea1106f00b3618a6d58de94192
Martin <slackgt@bigpond.com>
2025-03-06 08:04:16 +1100

    06032025-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
   // integrations: [relativeLinks()]
+  trailingSlash: "always",
+  site: "https://royalparkladiesgolfclub.com.au/",
+  integrations: [sitemap()],
 });
